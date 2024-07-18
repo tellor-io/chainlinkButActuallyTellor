d294c12a82bd7845843f6c50c5fceaa5ff157cf4
Nicholas Fett <nicholasfett@Nicholass-MacBook-Air.local>
2024-07-18 15:24:20 -0400

    fixes

--- src/ETHUSDPriceFeed.sol
+++ src/ETHUSDPriceFeed.sol
@@ -6,23 +6,22 @@ import "usingtellor/UsingTellor.sol";
 contract ETHUSDPriceFeed is UsingTellor {
     bytes queryData = abi.encode("SpotPrice", abi.encode("eth", "usd"));
     bytes32 ethUsdQueryId = keccak256(queryData);
-
-    constructor(address payable _tellor) UsingTellor(_tellor) {}
+    uint256 readDelay; //be sure to use the contract optimistically to allow time for disputes (e.g. 1 hours)
+    
+    constructor(address payable _tellor, uint256 _readDelay) UsingTellor(_tellor) {
+        readDelay = _readDelay;
+    }
 
     function latestRoundData()
         public
         view
         returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
     {
-        uint80 _roundId;
-        int256 _answer;
-        uint256 _startedAt;
         uint256 _updatedAt;
-        uint80 _answeredInRound;
         bytes memory _value;
-
-        (_value, _updatedAt) = getDataBefore(ethUsdQueryId, block.timestamp - 1 hours);
-        _answer = int256(abi.decode(_value, (uint256)));
-        return (_roundId, _answer, _startedAt, _updatedAt, _answeredInRound);
+        int256 price;
+        (_value, _updatedAt) = getDataBefore(ethUsdQueryId, block.timestamp - readDelay);
+        price = int256(abi.decode(_value, (uint256)));
+        return (0, price, _updatedAt, _updatedAt, 0);
     }
 }
---
+++ src/SecretTellorUser.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import "./interfaces/AggregatorV3Interface.sol";
+
+contract SecretTellorUser {
+    AggregatorV3Interface public ethOracle;
+    int256 public lastPrice;
+    uint256 public lastStoredTimestamp; // Cache timestamp to prevent dispute attacks
+
+    // Input tellor oracle address
+    constructor(address payable _ethOracle) {
+        ethOracle = AggregatorV3Interface(_ethOracle);
+    }
+
+    function setEthPrice()
+        public
+    {
+        int256 _value;
+        uint256 _timestampRetrieved;
+        // Retrieve data at least 15 minutes old to allow time for disputes
+        (,_value,_timestampRetrieved,,) = ethOracle.latestRoundData();
+        // If timestampRetrieved is 0, no data was found
+        if(_timestampRetrieved > 0) {
+            // Check that the data is not too old
+            if(block.timestamp - _timestampRetrieved < 24 hours) {
+                // Check that the data is newer than the last stored data to avoid dispute attacks
+                if(_timestampRetrieved > lastStoredTimestamp) {
+                    lastStoredTimestamp = _timestampRetrieved;
+                    lastPrice = _value;
+                }
+            }
+        }
+    }
+}
